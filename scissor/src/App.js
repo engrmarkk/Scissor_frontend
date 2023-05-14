56df8f39599f92ac23dbb3d36e0cf54c3b6e5a4d
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-14 19:44:58 +0100

    added the logout route

--- scissor/src/App.js
+++ scissor/src/App.js
@@ -8,15 +8,35 @@ import Home from "./Home";
 import Login from "./login";
 import Write from "./write";
 import Register from "./register";
+import UsersTable from "./users";
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const Token = localStorage.getItem("accessToken");
+  const [checkToken, setCheckToken] = useState(false);
+  useEffect(() => {
+
+      if(Token !== null){
+        setCheckToken(true) ;
+        console.log("Token is available")
+      } else {
+        setCheckToken(false) ;
+        console.log("Token is null")
+      }
+    
+  }, [Token])
+
+
+
   return (
       <div className="app-container">
-        <Home />
+        <Home isLoggedIn={checkToken}/>
         <Routes>
           <Route path="/login" element={<Login/>} />
           <Route path="/" element={<Write/>} />
           <Route path="/register" element={<Register/>} />
+          <Route path="/users" element={<UsersTable/>} />
         </Routes>
       </div>
   );
