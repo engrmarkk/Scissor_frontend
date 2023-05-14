2b739bd9152b1dd72d97bee4ebd206c07bb1816c
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-14 19:45:19 +0100

    logout link

--- scissor/src/Home.js
+++ scissor/src/Home.js
@@ -3,12 +3,13 @@ import React from "react";
 // different end points.
 import { Link } from "react-router-dom";
 // import Write from "./write";
+import { logout } from "./logout";
 
 
 
 
 
-const Home = () => {
+const Home = ({isLoggedIn}) => {
 return (
 	<div>
         <header className="App-header">
@@ -20,14 +21,30 @@ return (
 		{/* Endpoint to route to Home component */}
 		<Link to="/">Home</Link>
 		</li>
+
+        {!isLoggedIn && 
+        <>
 		<li>
-		{/* Endpoint to route to About component */}
+		{/* Endpoint to route to Login component */}
 		<Link to="/login">Login</Link>
 		</li>
 		<li>
-		{/* Endpoint to route to Contact Us component */}
+		{/* Endpoint to route to Register component */}
 		<Link to="/register">Register</Link>
 		</li>
+        <li>
+		{/* Endpoint to route to Register component */}
+		<Link to="/register">Contact Us</Link>
+		</li> </>}
+        <li>
+		{/* Endpoint to route to Register component */}
+		<Link to="/users">Users</Link>
+		</li>
+        {isLoggedIn && 
+        <li>
+		{/* Endpoint to route to Register component */}
+		<Link to="#" onClick={() => {logout()}}>Logout</Link> 
+		</li> }
 	</ul>
         </nav>
         </header>
