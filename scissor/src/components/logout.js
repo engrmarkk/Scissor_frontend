4ffa57a0f244da17d4a15b781092b32c7e5325c2
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-18 13:57:59 +0100

    just remove the access token from the local storage for the logout

--- scissor/src/components/logout.js
+++ scissor/src/components/logout.js
@@ -3,21 +3,23 @@ import axios from "axios";
 
 
 export const logout = () => {
-    const accessToken = localStorage.getItem("accessToken");
-    const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${accessToken}` };
+    // const accessToken = localStorage.getItem("accessToken");
+    // const headers = { 'Content-Type': 'application/json', 'Authorization': `Bearer ${accessToken}` };
   
-    axios.delete("http://localhost:5000/logout", {
-        headers,
-        withCredentials: true,
-      })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+    // axios.delete("http://localhost:5000/logout", {
+    //     headers,
+    //     withCredentials: true,
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //     window.location.href = "/";
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   })
 
       localStorage.removeItem("accessToken");
+      window.location.href = "/";
         setTimeout(() => {
           window.location.reload();             
         }, 1000);
