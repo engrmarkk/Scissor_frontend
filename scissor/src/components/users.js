2277ef8fb8e97b7f92d02757897fdfe1ffff95f9
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-18 14:00:30 +0100

    base url

--- scissor/src/components/users.js
+++ scissor/src/components/users.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../css/user.css'
-// import BASE_URL from './utils/constants';
-
-const BASE_URL = 'http://localhost:5000';
+import { BASE_URL } from '../utils/constants';
 
 function UsersTable() {
   const [users, setUsers] = useState([]);
