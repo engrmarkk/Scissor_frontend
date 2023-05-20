afe902f0515a5759baec9fe710833c3db0d60ad4
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-05-20 01:19:56 +0100

    display the user profile and activitiies

--- scissor/src/components/dashboard.js
+++ scissor/src/components/dashboard.js
@@ -1,72 +1,99 @@
 import React from "react";
 import "../css/dashboard.css";
 import { Link } from "react-router-dom";
-
+import { BASE_URL } from "../utils/constants";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import QRCode from 'qrcode.react';
 
 function Dashboard() {
-    return (
-      <div>
-        <h1>Dashboard</h1>
+  const [users, setUsers] = useState([]);
+  const [user_links, setUserLinks] = useState([]);
+  // const [dashboard, setDashboard] = useState([]);
+  // ...
 
-        <div className="user-profile">
-            <h2>User Profile</h2>
-            <p>Username: <span>admin</span></p>
-            <p>Email: <span>
-                <a href="mailto: s.com">s.com</a>
-            </span></p>
-            <p>Account Created: <span>2021-01-01</span></p>
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken"); // Get the token from local storage
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`, // Include the token in the authorization header with the "Bearer" prefix
+      },
+    };
 
+    axios
+      .get(`${BASE_URL}/dashboard`, config) // Fetch data from the first link
+      .then((response) => {
+        // Process the data
+        const users = response.data;
+        const user_links = response.data.user_links;
+        setUserLinks(user_links);
+        setUsers(users);
+        // ...
 
-        </div>
+        // Fetch data from another link within the same file
+        // return axios.get(`${BASE_URL}/all-link`, config);
+      })
+      // .then((response) => {
+      //   // Process the data from the other link
+      //   const otherData = response.data;
+      //   setDashboard(otherData);
+      // ...
+      // })
+      .catch((error) => console.error(error));
+  }, []);
 
-        <div className="shorten-url">
-            <Link to="/shorten-url" className="link-url">Shorten URL</Link>
-        </div>
-        
-        <div className="stats">
-          <h2>Stats</h2>
-          <p>Total URLs: 50</p>
-          <p>Shortened URLs: 30</p>
-          <p>Clicks: 100</p>
-        </div>
-        
-        <div className="recent-activity">
-          <h2>Recent Activity</h2>
-          <ul>
-            <li>Shortened "https://example.com"</li>
-            <li>Shortened "https://google.com"</li>
-            <li>Clicked on "https://example.com"</li>
-          </ul>
-        </div>
-        
-        <div className="url-list">
-          <h2>URL List</h2>
-          <ul>
-            <li>
-              <div>
-                <p>Original URL: <a href="https://example.com">https://example.com</a></p>
-                <p>Short URL: <a href="https://shorturl.com/abc123">https://shorturl.com/abc123</a></p>
-                <p>Clicks: 10</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <p>Original URL: <a href="https://google.com">https://google.com</a></p>
-                <p>Short URL: <a href="https://shorturl.com/xyz456">https://shorturl.com/xyz456</a></p>
-                <p>Clicks: 20</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <p>Original URL: <a href="https://example.org">https://example.org</a></p>
-                <p>Short URL: <a href="https://shorturl.com/def789">https://shorturl.com/def789</a></p>
-                <p>Clicks: 5</p>
-              </div>
-            </li>
-          </ul>
-        </div>
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <div className="user-profile">
+        <h2>User Profile</h2>
+        <p>
+          Username: <span>{users.username}</span>
+        </p>
+        <p>
+          Email:{" "}
+          <span>
+            <a href={`mailto:${users.email}`}>{users.email}</a>
+          </span>
+        </p>
+        <p>
+          First Name: <span>{users.first_name}</span>
+        </p>
+        <p>
+          Last Name: <span>{users.last_name}</span>
+        </p>
       </div>
-    );
-  }
+
+      <div className="shorten-url">
+        <Link to="/shorten-url" className="link-url">
+          Shorten URL
+        </Link>
+      </div>
+
+      <div className="url-list">
+        <h2>URL List</h2>
+        <ul>
+          {user_links.map((item) => (
+            <li key={item.id}>
+              <div>
+                <p>
+                  Original URL: <a href={item.url}>{item.url}</a>
+                </p>
+                <p>
+                  Short URL: <a href={item.short_url}>{item.short_url}</a>
+                </p>
+                <p>
+                  <img src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${item.short_url}`} alt="QR Code" />
+                </p>
+                <p>Clicks: {item.visit}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default Dashboard;
